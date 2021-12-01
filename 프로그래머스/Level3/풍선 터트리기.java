54acd6d64a3adec876f364c93746764715617f7f
SpectacleBlind <kekpnny@gmail.com>
2021-12-02 02:41:40 +0900

    프로그래머스 Level 3 - 풍선 터트리기

---
+++ 프로그래머스/Level3/풍선 터트리기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] a) {
+       int answer = 2; // 처음과 끝은 무조건 남길 수 있다.
+       int l = a[0], r = a[a.length-1];
+       for (int i = 1; i < a.length-1; i++) { // 가운데 풍선을 걸러낸다.
+		if(l > a[i]) {
+			l = a[i]; 
+			answer++;
+		}
+		if(r > a[a.length-1-i]) {
+			r = a[a.length-1-i];
+			answer++;
+		}
+	}
+        return l ==r ? answer-1 : answer; // l과 r이 같으면 중복 발생
+    }
+}
