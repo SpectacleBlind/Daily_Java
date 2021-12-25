ddbb5e41632c8c385f6579303623eac2f3fe145c
SpectacleBlind <kekpnny@gmail.com>
2021-12-26 00:09:25 +0900

    프로그래머스 Level 1 K 번째 수

---
+++ 파이썬/Level 1/K 번째 수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    
+    for i in commands:
+        slicing = sorted(array[i[0]-1:i[1]])
+        answer.append(slicing[i[2]-1])
+    return answer
