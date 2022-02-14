954aa9de20ff3e91bac17b12e85482aff4b11435
SpectacleBlind <kekpnny@gmail.com>
2022-02-15 00:57:53 +0900

    프로그래머스 Level 2 - 모음사전

---
+++ 파이썬/Level 2/모음사전.py
@@ -0,0 +1,13 @@
+from itertools import product
+
+def solution(word):
+
+    
+    dict = []
+    for i in range(1,6) :
+        dict += list(map(''.join, product("AEIOU", repeat = i)))
+    dict.sort()
+    
+    answer = dict.index(word) + 1
+    
+    return answer
